edf08cbebf63eb4319d39afda80747fb1b7aa968
Mohammad Azharuddin <azharuddinm2211@gmail.com>
2021-01-06 03:39:56 +0530

    ..

--- mbot/dialog-manager/handler.js
+++ mbot/dialog-manager/handler.js
@@ -1,6 +1,7 @@
 const responses = require("../dialog-manager/responses");
 // requireAll actions from a dir
 const modules = require('require-dir-all')("../dialog-manager/actions");
+const {sendAlertMail} = require("../utils/mail");
 const actions = {};
 let keys = Object.keys(modules);
 for(let i=0;i<keys.length;i++){
@@ -20,6 +21,7 @@ module.exports.handleIntents = async (userObj, intent) => {
     catch(err){
         console.error('ERROR DURING RESPONDING TO INTENT');
         console.error(err);
+        sendAlertMail(err.stack);
         return [];
     }
 
